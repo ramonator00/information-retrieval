d67985bf7ae3e8906cd8d72966d6c1fee6386ddd
ramona.koksa@students.ffhs.ch <ramona.koksa@students.ffhs.ch>
2022-10-08 22:56:45 +0200

    add date

--- data-cleaning/DataCleaner.py
+++ data-cleaning/DataCleaner.py
@@ -1,6 +1,6 @@
-from datetime import datetime, timedelta
+from datetime import datetime
+import numpy as np
 import pandas as pd
-import random
 import sqlite3
 
 # This script is used to harmonize the data into a sqlite database
@@ -11,9 +11,7 @@ sqlite_database = '../corpus/data.db'
 en_recipes = '../corpus/recipes_en.csv'
 de_recipes = '../corpus/recipes_de.json'
 
-start = datetime.now()
-end = start - timedelta(days=1000)
-random_date = start + (end - start) * random.random()
+year = datetime.today().year
 
 con = sqlite3.connect(sqlite_database)
 cur = con.cursor()
@@ -25,7 +23,7 @@ new_de_df['ID'] = de_df.index + 500000
 new_de_df['TITLE'] = de_df['Name']
 new_de_df['INGREDIENTS'] = str(de_df['Ingredients'])
 new_de_df['INSTRUCTIONS'] = de_df['Instructions']
-new_de_df['R_DATE'] = de_df['Year']
+new_de_df['R_DATE'] = np.random.choice(pd.date_range('1980-01-01', '2000-01-01'), len(new_de_df))
 new_de_df['LANGUAGE'] = 0  # German
 
 # write new df to sql
@@ -38,7 +36,7 @@ new_en_df['ID'] = en_df.index + 10000
 new_en_df['TITLE'] = en_df['Title']
 new_en_df['INGREDIENTS'] = en_df['Ingredients']
 new_en_df['INSTRUCTIONS'] = en_df['Instructions']
-new_en_df['R_DATE'] = random_date
+new_en_df['R_DATE'] = np.random.choice(pd.date_range('1980-01-01', '2000-01-01'), len(new_en_df))
 new_en_df['LANGUAGE'] = 1  # English
 
 # write new df to sql
